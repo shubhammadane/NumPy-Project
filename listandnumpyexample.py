3404dd178208ba9e841bb6a7fb17c94d68da4d44
Shubham Madane <shubhammadane@gmail.com>
2026-01-23 12:32:56 +0530

    list and Numpy

---
+++ listandnumpyexample.py
@@ -0,0 +1,6 @@
+list = ["Shubham","Ram","Krushna","Aniket","Sanket" ]
+print("Names of Students:", list)   
+
+import numpy as np
+list2 = np.array(["Shubham","Ram","Krushna","Aniket","Sanket"])
+print("Names of Students (using numpy):", list2)
